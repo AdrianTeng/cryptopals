6cc8fce4e9ded7408d4293b31a13eb3aa011ed49
AdrianTeng <adrian@teng.io>
2014-08-20 23:38:19 +0100

    Update travis yaml for dependency

--- setup.py
+++ setup.py
@@ -1,13 +1,19 @@
 from distutils.core import setup
+from pip.req import parse_requirements
+
+
+install_reqs = parse_requirements("requirements.txt")
+reqs = [str(ir.req) for ir in install_reqs]
+
 
 setup(
     name='Cryptopals',
-    version='',
+    version='1.0',
     packages=['ateng', 'tests'],
-    url='',
+    url='http://github.com/AdrianTeng/Cryptopals',
     license='',
     author='ateng',
     author_email='adrian@teng.io',
-    description='',
-    install_requires=['pycrypto>=2.6']
+    description='Cryptopals solution in Python',
+    install_requires=reqs
 )
