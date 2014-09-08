6178e7c651ff3f31535f47362945520cd80369ec
AdrianTeng <adrian@teng.io>
2014-09-08 10:05:21 +0800

    Refactor of Bytes class

--- ateng/bytes.py
+++ ateng/bytes.py
@@ -1,22 +1,30 @@
-__author__ = 'ateng'
-
-
-def Bytes(value=b''):
-    if isinstance(value, str):
-        value = map(ord, value)
-    return _Bytes(value)
-
-class _Bytes(bytes):
-
-    def _pad(self, other):
-        s = len(self)
-        o = len(other)
-        if s == o:
-            return zip(self, other)
-        if s > o:
-            return zip(self, (other * (s // o)) + (other[0:s%o]))
-        if o > s:
-            return zip((self * (o // s)) + (self[0:o%s]), other)
-
+from itertools import cycle, islice
+ 
+ 
+class Bytes(bytes):
+    def __new__(cls, value):
+        # We can't override __init__ as bytes is immutable.
+        # See https://docs.python.org/3/reference/datamodel.html#object.__new__
+        if isinstance(value, str):
+            value = map(ord, value)
+ 
+        return super().__new__(cls, value)
+ 
+    def _zip_longest(self, other):
+        """Zip this byte sequence with another. If they're not the same
+        length, repeat the items from the shorter sequence.
+ 
+        If you just want to zero-pad the shorter byte sequence, you
+        can do this instead:
+ 
+        >>> itertools.zip_longest(self, other, fillvallue=0)
+ 
+        """
+        length = max(len(self), len(other))
+        # We can't use
+        # >>> zip(cycle(self), cycle(other))[:length]
+        # here because zip doesn't return a list in Python 3.
+        return islice(zip(cycle(self), cycle(other)), length)
+ 
     def __xor__(self, other):
-        return Bytes([i1 ^ i2 for (i1, i2) in self._pad(other)])
+        return Bytes([i1 ^ i2 for (i1, i2) in self._zip_longest(other)])
